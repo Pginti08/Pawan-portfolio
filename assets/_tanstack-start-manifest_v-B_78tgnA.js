08d0eacff4b8355043ac2cc3c908196797a03463
Pawan-tech1 <pawan@vrozart.com>
2026-04-16 23:02:48 +0530

    Deploy setup fixed for GitHub pages

--- assets/_tanstack-start-manifest_v-B_78tgnA.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/pawanginti/Desktop/Pawan_ginti-web/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-qb1Io0EP.js"] }, "/": { filePath: "/Users/pawanginti/Desktop/Pawan_ginti-web/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-DuJE-hkq.js"] } }, clientEntry: "/assets/index-qb1Io0EP.js" });
-export {
-  tsrStartManifest
-};
